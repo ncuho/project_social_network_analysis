50ee7a9a3992bd48e3f3465988adf50f5d3ae93d
Vladimir Grichenko <poch961g@gmail.com>
2024-04-04 16:12:11 +0300

    Добавляется работа с таблицей Link

--- SocialNetworkAnalysis/main/views.py
+++ SocialNetworkAnalysis/main/views.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from .account.forms import LoginForm, UserRegistrationForm
+from .account.forms import LoginForm
+#from .models import Link
 
 
 def index(request):
+    #print(Link.objects.all());
     if not request.user.is_authenticated:
         return redirect('%s?next=%s' % ("login", request.path))
     if request.method == 'POST':
@@ -18,6 +20,7 @@ def index(request):
 
 def login_views(request):
     if request.method == 'POST':
+
         print(User.objects.all())
         form = LoginForm(request.POST)
         if form.is_valid():
