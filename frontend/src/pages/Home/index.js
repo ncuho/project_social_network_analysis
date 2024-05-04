806db77e4969f3dd5fe451cda1c42fe2b4b5e6f8
Nikita Surovegin <nikitamasonskij@gmail.com>
2024-05-04 20:49:16 +0300

    Добавил фронтенд

---
+++ frontend/src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import classes from './Home.module.css'
+
+function Home() {
+    return(
+        <div className={classes.page_home}>
+            <div className="indent container">
+                <h1>Home</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home
